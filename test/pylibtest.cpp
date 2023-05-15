86a195f3a64919f13452f4fe56f003306066b19a
Daniele Rapetti <daniele.rapetti@sissa.it>
2023-05-15 17:02:10 +0200

    set up the test, need to understand how to pass by reference

--- test/pylibtest.cpp
+++ test/pylibtest.cpp
@@ -26,3 +26,30 @@ TEST_CASE("Playing with a dictionary", "[py]") {
     REQUIRE(d["test"].cast<int>() == i);
   }
 }
+
+PYBIND11_EMBEDDED_MODULE(embeddedMylib, m) {
+  m.doc() = "mylib"; // optional module docstring
+
+  pybind11::class_<mylib::SimpleIO>(m, "SimpleIO")
+      .def("getI", &mylib::SimpleIO::getI)
+      .def("setI", &mylib::SimpleIO::setI);
+}
+
+TEST_CASE("Playing with embedded my lib", "[py]") {
+  py::scoped_interpreter guard{};
+  auto embeddedMylib = py::module_::import("embeddedMylib");
+  auto py_module = py::module::import("pylibtest");
+  auto py_fcn = py_module.attr("setter");
+
+  SECTION("Creating and passing a SimpleIObject") {
+    mylib::SimpleIO mytest;
+    // auto mytest = embeddedMylib.attr("SimpleIO");
+    // the name of the variables help reading the tests errors
+    mytest.setI(42);
+    int prevInCpp = mytest.getI();
+    auto setValue = GENERATE(take(100, random(-100, 100)));
+    auto prevInPy = py_fcn(mytest, setValue);
+    REQUIRE(prevInPy.cast<int>() == prevInCpp);
+    REQUIRE(mytest.getI() == setValue);
+  }
+}
--- test/pylibtest.py
+++ test/pylibtest.py
@@ -1,5 +1,9 @@
-def setter(objectToSet, value):
+import embeddedMylib
+
+def setter(objectToSet:embeddedMylib.SimpleIO, value):
+    toret = objectToSet.getI()
     objectToSet.setI(value)
+    return toret
 
 def setterdict(dictToset:dict,name, value):
     dictToset[name]=value
